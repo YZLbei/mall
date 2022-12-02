96898a8420b26b3a4da07e6fedd45a6bc99ab660
YZLBei <yuzhenlongbei@outlook.com>
2022-12-02 21:13:03 +0800

    项目初始结构创建

---
+++ mall-product/src/test/java/com/yu/product/MallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
